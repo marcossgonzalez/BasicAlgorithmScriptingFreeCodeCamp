6792b81b2e1c92a6a950da8313766f4cf5ff80e4
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-09 01:20:28 -0700

    Update lesson
    Update lesson

--- basicalgorithms.js
+++ basicalgorithms.js
@@ -17,3 +17,15 @@ function reverseString(str) {
   
   reverseString("hello");
 //-------------------------------------------
+function factorialize(num) {
+    if (num == 0){
+    return 1;
+    }
+    else{
+      return num * factorialize(num -1); 
+    }
+    
+  }
+  
+factorialize(5);
+//-------------------------------------------
